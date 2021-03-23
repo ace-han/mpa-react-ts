927f12658d274f195ea4fba08b7c785751ae4a58
Ace <ace.jl.han@gmail.com>
2021-03-23 23:55:53 +0800

    django template framework first attemp.
    there is something I need to figure out about `webpack-node-externals`

--- client/.neutrinorc.js
+++ client/.neutrinorc.js
@@ -32,5 +32,23 @@ module.exports = {
       // devServer: { proxy: { '/api': 'http://localhost:8000' } },
     }),
     jest(),
+    //
+    // Ensure that react is read from global - and webpack-node-externals is NOT used.
+    // 
+    // By default the react-components plugin uses webpack-node-externals to build
+    // the externals object. This will simply get all dependencies and assume they are
+    // external AND assume that requirejs is used.
+    // 
+    // However, for a web usage, we want only some external dependencies set up and
+    // want them to read from global (aka root), hence we map the 'react' import to 'React' global.
+    // See:
+    //  
+    // https://www.npmjs.com/package/webpack-node-externals
+    // https://webpack.js.org/configuration/externals/#externals
+    neutrino => {
+      neutrino.config.when(process.env.NODE_ENV === 'production', config => {
+        config.externals({ react: 'React' });
+      });
+    },
   ],
 };
